f7b6e173bc5d9d28093e17a72544df9475f06a88
Brigid Horan <brigid.horan@tufts.edu>
2018-10-19 15:04:24 -0400

    Map now loads! Index, js and css

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -1,1 +1,17 @@
-/*Generic JavaScript outline*/
+
+
+function initMap() {
+
+
+	var myOptions = {
+	center: {lat: 42.352271, lng: -71.05524200000001},
+	zoom: 13,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+	}	
+
+	var map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+	var myLat = 0;
+	var myLng = 0;
+	var me = new google.maps.LatLng(myLat, myLng)
+}
