e84e7b32f11968416e2709c45094ff6d27f38a7f
Brigid Horan <brigid.horan@tufts.edu>
2018-10-20 12:24:32 -0400

    Marker with infowindow for geolocation

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -1,17 +1,55 @@
+var myLat = 0;
+var myLng = 0;
+var me = new google.maps.LatLng(myLat, myLng)
 
+var map;
 
 function initMap() {
 
-
 	var myOptions = {
-	center: {lat: 42.352271, lng: -71.05524200000001},
-	zoom: 13,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
+		//South Station center
+		center: {lat: 42.352271, lng: -71.05524200000001},
+		zoom: 13,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}	
 
-	var map = new google.maps.Map(document.getElementById('map'), myOptions);
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+//	setTstops(map);
+	getMyLocation();
+}
 
-	var myLat = 0;
-	var myLng = 0;
-	var me = new google.maps.LatLng(myLat, myLng)
+function getMyLocation() {
+	//If supported on browser (yes for chrome)
+	if (navigator.geolocation) { 
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			renderMap();
+		});
+	}
+	else {
+		alert("Geolocation is not supported by your web browser. RIP.");
+	}
+}
+
+function renderMap() {
+	me = new google.maps.LatLng(myLat, myLng);
+
+	// Update map from SS to geolocation
+	map.panTo(me);
+	
+	// Create a marker
+	marker = new google.maps.Marker({
+		position: me,
+		title: "You are Here!"
+	});
+	marker.setMap(map);
+		
+	// Open info window on click of marker
+	google.maps.event.addListener(marker, 'click', function() {
+		var infowindow = new google.maps.InfoWindow({
+  content: marker.title});
+		//infowindow.setContent(marker.title);
+		infowindow.open(map, marker);
+	});
 }
